7910aab429c38cd94d57554dd2c7d193ff7cee8f
Swadesh13 <swadeshjana@gmail.com>
2021-07-07 00:04:43 +0530

    Indicators Updated

--- src/indicators/rsi.py
+++ src/indicators/rsi.py
@@ -2,25 +2,25 @@ import numpy as np
 from typing import Union
 
 
-def get_cmo(close: Union[np.ndarray, list], period: int) -> float:
+def get_rsi(close: Union[np.ndarray, list], period: int) -> float:
     """
-    Calculate the CMO for a given period. 
+    Calculate the RSI for a given period. 
     Refer - https://www.fmlabs.com/reference/default.htm?url=CMO.htm
 
     Arguments:
     close - list or numpy array of close prices, latest last (highest index)
-    period - period for which CMO has to be calculated
+    period - period for which RSI has to be calculated
 
     Returns:
-    CMO of the given close values
+    RSI of the given close values
     """
     assert len(close) > period
     current_close = np.array(close[-period:])
     prev_close = np.array(close[-period-1:-1])
     diff = current_close-prev_close
 
-    num = np.sum(diff)
+    num = np.sum(np.clip(diff, 0, np.Inf))
     denum = np.sum(np.abs(diff))
 
-    cmo = 100*num/denum
-    return cmo
+    rsi = 100*num/denum
+    return rsi
